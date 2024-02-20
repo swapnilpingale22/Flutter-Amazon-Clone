133549461590672584f2e9747771b9cc9def1a60
Swapnil <swapnilpingale22@gmail.com>
2024-02-20 11:37:35 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 //import from other files
 const authRouter = require("./routes/auth");
@@ -10,10 +11,12 @@ const userRouter = require("./routes/user");
 //init
 const PORT = process.env.PORT || 3000;
 const app = express();
-const Url = "mongodb+srv://swapnil:Swapnil123@cluster0.dtqtzbq.mongodb.net/?retryWrites=true&w=majority"
+const Url =
+  "mongodb+srv://swapnil:Swapnil123@cluster0.dtqtzbq.mongodb.net/?retryWrites=true&w=majority";
 // const localUrl = "mongodb://127.0.0.1:27017/Amazon_Clone_Db"
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
@@ -21,14 +24,15 @@ app.use(productRouter);
 app.use(userRouter);
 
 //connections
-mongoose.connect(Url)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+mongoose
+  .connect(Url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server  is running on ${PORT}`);
+  console.log(`Server  is running on ${PORT}`);
 });
